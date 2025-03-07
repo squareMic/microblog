b3bf3cece65ff951a6c5534200f284d6ca189225
Mic <michaelmay179@gmail.com>
2025-03-07 15:19:05 +0100

    Add receiving login credentials and flash messages

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -36,4 +36,7 @@ def index():
 @app.route('/login')
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
